33abb23624e408108f96341a0679c790d6e8d5a9
Jeddy <jeddyranivo@gmail.com>
2023-11-21 10:15:27 +0200

    modeles et repository

--- src/main/java/nba/s5_nba_back/repository/ActionRepository.java
+++ src/main/java/nba/s5_nba_back/repository/ActionRepository.java
@@ -3,6 +3,8 @@ package nba.s5_nba_back.repository;
 import nba.s5_nba_back.model.Action;
 import nba.s5_nba_back.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ActionRepository extends JpaRepository<Action,Long> {
+    //@Query
 }
