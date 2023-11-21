a0c5e0cfc8b83c866e05e5885b7638d93c14ec52
priscafehiarisoa <mytyrealy@gmail.com>
2023-11-21 09:48:11 +0300

    initial commit

---
+++ src/main/java/nba/s5_nba_back/S5NbaBackApplication.java
@@ -0,0 +1,13 @@
+package nba.s5_nba_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5NbaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S5NbaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nba/s5_nba_back/S5NbaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package nba.s5_nba_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5NbaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
